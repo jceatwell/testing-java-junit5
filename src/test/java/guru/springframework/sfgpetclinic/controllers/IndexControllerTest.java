3e1247ddac2f46f295840e7a970b214ef07d7d14
John Eatwell <jceatwell@gmail.com>
2021-11-28 07:11:00 +0200

    added tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index(), "Wrong view returned");
+    }
+
+    @Test
+    void oupsHandler() {
+    }
+}
